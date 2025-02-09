4b729ca536cb4084c743e8166a667f306d75b41d
Angel Chicote <angel.chiveg@educa.jcyl.es>
2025-02-09 10:38:10 +0100

    Creación del proyecto.

---
+++ main.py
@@ -0,0 +1,23 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', port=8000, reload=True)
+
+
+# Docs
+# http://127.0.0.1:8000/docs
+# http://127.0.0.1:8000/redoc
