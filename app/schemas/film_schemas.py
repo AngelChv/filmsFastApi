96abad1e2abd82cd5e894b9ac79f5bcab80cdede
Angel Chicote <angel.chiveg@educa.jcyl.es>
2025-02-09 12:32:31 +0100

    Esquemas de película.

---
+++ app/schemas/film_schemas.py
@@ -0,0 +1,45 @@
+from pydantic import BaseModel, EmailStr
+
+
+class FilmCreate(BaseModel):
+    """
+    Representa los datos que se envían en la petición para crear una nueva película.
+    """
+    title: str
+    director: str
+    year: int
+    duration: int
+    description: str
+    poster_path: str
+
+
+class FilmUpdate(BaseModel):
+    """
+    Representa los datos que se envían en la petición para editar una película.
+    """
+    id: int
+    title: str
+    director: str
+    year: int
+    duration: int
+    description: str
+    poster_path: str
+
+
+class FilmResponse(BaseModel):
+    """
+    Representa los datos enviados como respuesta de una petición a la api
+    """
+    id: int
+    title: str
+    director: str
+    year: int
+    duration: int
+    description: str
+    poster_path: str
+
+    # Es necesario para trabajar con pydantic y SQLAlchemy
+    # Esto indica a pydantic que al instanciar la clase se van a usar atributos de la clase modelo en lugar
+    # de un diccionario
+    class Config:
+        from_attributes = True
